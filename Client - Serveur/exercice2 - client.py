095e0b4eab1bdd03bd4d839aa40d4a07b94546ab
BuildTools <unconfigured@null.spigotmc.org>
2023-11-12 02:54:48 +0100

    Gestions erreurs basiques (arret + bye) -> ok

--- Client - Serveur/exercice2 - client.py
+++ Client - Serveur/exercice2 - client.py
@@ -2,6 +2,10 @@ import socket
 import sys
 import random
 import threading
+import select
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True) # pour que les couleurs s'appliquent à tout le terminal
 
 #host = "127.0.0.1"
 #port = 1234
@@ -10,6 +14,10 @@ global connected
 connected = False
 
 class ChallengeRefused(Exception): # erreur customisée en lien avec le challenge
+    def __init__(self, message):
+        super().__init__(message)
+        
+class ConnectionClosedByServer(Exception):
     def __init__(self, message):
         super().__init__(message)
 
@@ -24,46 +32,69 @@ def main(host="127.0.0.1", port=1234):
             raise ChallengeRefused("You're not allowed to access to this server...")
     except ChallengeRefused as err:
         print("Erreur :", err)
+    except ConnectionRefusedError as err:
+        print(Fore.RED + " \nLa connexion n'a pas aboutie, vérifiez que le serveur est bien lancé et que l'adresse est correcte")
     else:
-        print("You're connected to the server !")
+        print(Fore.GREEN + "You're connected to the server !")
         interactive(host)
         
     return 0 #print(host,port,challenge)
 
 def send(socket, host):
     global connected
+    global msg
     while connected:
-        msg = input(f"you@{host} $:")
-        socket.send(msg.encode()) if msg else None
-        if msg == "bye" or msg == "arret":
-            connected = False
-            socket.close()
+        try:
+            msg = input(f"you@{host} $:")
+            socket.send(msg.encode()) if msg else None
+            if msg == "bye" or msg == "arret":
+                connected = False
+                socket.close()
+                break
+            msg = None
+        except ConnectionAbortedError as err:
+            print(Fore.RED + "La connexion a été interrompue par le serveur") \
+                if not msg == "bye" \
+                    else print(Fore.GREEN + "Vous avez bien été déconnecté du serveur")
             break
-        msg = None
-        
+        else: 
+            pass       
     return 0
 
 def receive(socket, host):
     global connected
+    global msg # récupérer le dernier msg envoyé par le client
     while connected:
-        reply = socket.recv(1024).decode()
-        print(f"\nserver :",reply) if reply else None
-        if reply == "bye" or reply == "arret":
-            connected = False
-            socket.close()
+        try:
+            reply = socket.recv(1024).decode()
+            print(f"\nserver :",reply) if reply else None
+            if reply == "bye" or reply == "arret":
+                connected = False
+                socket.close()
+                raise ConnectionClosedByServer('closed connection')
+                break
+            reply = None
+        except ConnectionClosedByServer as err:
+            print(Fore.GREEN + "Vous avez bien été déconnecté du serveur")
             break
-        reply = None
+        except ConnectionAbortedError as err:
+            print(Fore.RED + "La connexion a été interrompue par le serveur") \
+                if not msg == "bye" \
+                    else print(Fore.GREEN + "Vous avez bien été déconnecté du serveur")
+            break
+        else: 
+            pass
+        
     return 0
 
 def interactive(host):
     global connected
     connected = True
     print("Interactive mode")
+    #send(client_socket,host) # ne pas le mettre dans un thread pour qu'il puisse gérer activement l'input
     threading.Thread(target=receive, args=(client_socket,host)).start()
-    threading.Thread(target=send, args=(client_socket,host)).start()
-    
-    
-    
+    send(client_socket,host) # ne pas le mettre dans un thread pour qu'il puisse gérer activement l'input
+    #threading.Thread(target=send, args=(client_socket,host)).start()
     """     while connected:
         message = input(f"you@{host} $:")
         print(f"you@{host} $:", message) if message else None
--- Client - Serveur/exercice2 - server.py
+++ Client - Serveur/exercice2 - server.py
@@ -5,6 +5,9 @@ host = "0.0.0.0"
 port = 1234
 #reply = "synced"
 #message = "Hello World !"
+global connected 
+connected = True
+
 def send(socket, host):
     global connected
     while connected:
@@ -12,7 +15,7 @@ def send(socket, host):
         socket.send(msg.encode()) if msg else None
         if msg == "arret":
             connected = False
-            socket.close()
+            #socket.close()
             break
         msg = None
     return 0
@@ -20,13 +23,18 @@ def send(socket, host):
 def receive(socket, host):
     global connected
     while connected:
-        reply = socket.recv(1024).decode()
-        print(f"\nclient :",reply) if reply else None
-        if reply == "arret":
-            connected = False
-            socket.close()
+        try:
+            reply = socket.recv(1024).decode()
+        except ConnectionResetError:
+            print("La connexion a été interrompue par le client")
             break
-        reply = None    
+        else:
+            print(f"\nclient :",reply) if reply else None
+            if reply == "arret":
+                connected = False
+                #socket.close()
+                break
+            reply = None    
     return 0
 
 def interactive(target, host):
@@ -34,8 +42,9 @@ def interactive(target, host):
     connected = True
     print("Connected to client")
     threading.Thread(target=receive, args=(target,host)).start()
-    threading.Thread(target=send, args=(target,host)).start()
-    
+    #threading.Thread(target=send, args=(target,host)).start()
+    send(target,host) # ne pas le mettre dans un thread pour qu'il puisse gérer activement l'input
+    return 0
     """ while connected:
         reply = target.recv(1024).decode()
         print(f"client@{host} $:", reply) if reply else None
@@ -51,7 +60,7 @@ server_socket.bind((host, port))
 server_socket.listen(5)
 
 
-while True:
+while connected: # remplacer shutdown plutôt
     conn, address = server_socket.accept()
     message = conn.recv(1024).decode()
     client_challenge = message.split(",")
@@ -59,12 +68,14 @@ while True:
     client_condition = all(connect_condition(elem) for elem in client_challenge)
     if client_condition:
         conn.send("synced".encode())
-        threading.Thread(target=interactive, args=(conn,host)).start()
-        #interactive(conn)
+        #threading.Thread(target=interactive, args=(conn,host)).start()
+        interactive(conn, host)
+        conn.close()
         #conn.close()
     else:
         conn.send("garbage".encode())
         conn.close()
+
 
 
 
